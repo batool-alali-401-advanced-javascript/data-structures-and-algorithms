9be32d188bdb84fc9d632ad66c79ecb6db59d514
batoolalali <abdullahbatool93@gmail.com>
2020-06-15 21:00:39 +0300

    try with code challenge 17

---
+++ Data-Structures/tree/breadth-first.test.js
@@ -0,0 +1,24 @@
+'use strict';
+const Node= require('./tree.js').Node;
+const BST = require('./tree.js').BinaryTree;
+
+describe('Breadth first Traversal Tree Module', ()=>{
+  
+ 
+  
+  it('sssss', ()=>{
+    let tree = [];
+    let one = new Node(10);
+    let two = new Node(20);
+    let three = new Node(30);
+    let four = new Node(40);
+
+    one.left = three;
+    one.right = two;
+    two.left = four;
+
+    tree = new BST(one);
+    expect(tree.breadthFirst()).toEqual([10,30,20,40]);
+  });
+  
+});
